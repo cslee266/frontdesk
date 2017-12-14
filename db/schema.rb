44c91b5cead807a167c9490fb930aacd27318aea
Chin Seng Lee <chinseng@Chins-MacBook-Air.local>
2017-12-14 15:24:52 +0800

    added units

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214062022) do
+ActiveRecord::Schema.define(version: 20171214063130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20171214062022) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_authentications_on_user_id", using: :btree
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.string   "residence"
+    t.string   "block"
+    t.integer  "floor"
+    t.string   "unit_number"
+    t.string   "unit_type"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
