78b7e9656274cb27c72e588c5476314cce5a24f9
Chin Seng Lee <chinseng@Chins-MacBook-Air.local>
2017-12-14 11:34:35 +0800

    installed omniauth fb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,28 @@
-class UsersController < ApplicationController
+class UsersController < Clearance::UsersController
 	before_action :require_login
+	before_action :find_user, only: [:show, :edit, :update]
+
+	def edit
+	end
+
+	def show
+
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+	end
+
+	private
+	def find_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:email, :name, :password, :avatar)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   get "/sign_up" => "clearance/users#new", as: "sign_up"
 
   root "welcome#index"
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
