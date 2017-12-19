be92bc5886c6252b684b89aaf57cb6c2822759cc
Chin Seng Lee <chinseng@Chins-MacBook-Air.local>
2017-12-19 16:23:38 +0800

    news api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   
   require 'open_weather' 
+
+  require 'open-uri'
+
+	url = 'https://newsapi.org/v2/top-headlines?'\
+	      'sources=bbc-news&'\
+	      'apiKey=ff5aca1a4edf4ee1ac56620366445fee'
+
+	req = open(url)
+	response_body = req.read
+	puts response_body
 end
